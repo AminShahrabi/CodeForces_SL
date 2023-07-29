49413d5e345e4ff25fc41f0846ed4ba730ea62dc
Amin Shahrabi <105936809+AminShahrabi@users.noreply.github.com>
2023-07-29 08:53:21 +0330

    Create 158A - Next Round.cpp

---
+++ C++/158A - Next Round.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+ 
+using namespace std;
+int main()
+{
+    int n, k, ans;
+    cin >> n >> k;
+    int a[n];
+    
+    for (int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+    
+    for (int i = 0; i < n; i++){
+        if (a[i] >= a[k - 1] && a[i] != 0){
+            ans++;
+        }
+    }
+    cout << ans;
+    return 0;
+}
