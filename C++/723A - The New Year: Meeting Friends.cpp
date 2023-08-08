f7b421de2e63913e8b59c3aaf3aef3c6b97206dc
Amin Shahrabi <105936809+AminShahrabi@users.noreply.github.com>
2023-08-08 14:14:08 +0330

    Create 723A - The New Year: Meeting Friends.cpp

---
+++ C++/723A - The New Year: Meeting Friends.cpp
@@ -0,0 +1,11 @@
+// Amin Shahrabi Github
+// https://github.com/AminShahrabi/CodeForces_Solutions
+// Shush Gharbi
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	int a, b, c;
+	cin >> a >> b >> c;
+	cout << max(a, (max(b, c))) - min(a, min (b, c));
+}
