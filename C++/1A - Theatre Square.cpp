13fd165ca17ab55203c0c72120f271440b2db7dc
Amin Shahrabi <105936809+AminShahrabi@users.noreply.github.com>
2023-08-10 19:17:30 +0330

    Create 1A - Theatre Square.cpp

---
+++ C++/1A - Theatre Square.cpp
@@ -0,0 +1,13 @@
+// Amin Shahrabi Github
+// https://github.com/AminShahrabi/CodeForces_Solutions
+// Shush Gharbi
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	double m, n, a;
+	cin >> n >> m >> a;
+	
+	cout << (long long) ceil(n / a) * (long long) ceil(m / a);
+	
+}
