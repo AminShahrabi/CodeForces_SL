643ddd378621a2e0bad44a7a2415b54dd1f28fcd
Amin Shahrabi <105936809+AminShahrabi@users.noreply.github.com>
2023-08-10 19:55:44 +0330

    Create 122A - Lucky Division.cpp

---
+++ C++/122A - Lucky Division.cpp
@@ -0,0 +1,15 @@
+// Amin Shahrabi Github
+// https://github.com/AminShahrabi/CodeForces_Solutions
+// Shush Gharbi
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	int babat;
+	cin >> babat;
+	if( babat % 4 == 0 || babat % 7 == 0 || babat % 44 == 0 || babat % 47 == 0 || babat % 74 == 0 || babat % 77 == 0 || babat % 444 == 0 || babat % 447 == 0 || babat % 474 == 0 || babat % 744 == 0 || babat % 477 == 0 || babat % 747 == 0 || babat % 774 == 0 || babat % 777 == 0){
+		cout << "YES";
+	}
+	else cout << "NO";
+	
+}
