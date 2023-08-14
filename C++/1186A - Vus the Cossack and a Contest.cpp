8445d709aceb8f5de586761e613ad9d8cf482971
Amin Shahrabi <105936809+AminShahrabi@users.noreply.github.com>
2023-08-14 22:42:32 +0330

    Create 1186A - Vus the Cossack and a Contest.cpp

---
+++ C++/1186A - Vus the Cossack and a Contest.cpp
@@ -0,0 +1,17 @@
+// Amin Shahrabi Github
+// https://github.com/AminShahrabi/CodeForces_Solutions
+// Shush Gharbi
+
+#include <bits/stdc++.h>
+using namespace std;
+          
+int main(){
+	int a, b, c;
+	cin >> a >> b >> c;
+	
+	if (b >= a && c >= a ) cout << "Yes";
+	else cout << "No";
+}   
+
+
+ 
